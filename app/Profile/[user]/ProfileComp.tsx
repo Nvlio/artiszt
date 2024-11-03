a2e7ac9ea93f5b3fd0fdee625c8b9eb8b5da47dd
Nvlio <jgabriell2009@gmail.com>
2024-11-03 16:50:25 -0300

    Arrumado alguns problemas, e adicionado sistema de seguidores

--- app/Profile/[user]/ProfileComp.tsx
+++ app/Profile/[user]/ProfileComp.tsx
@@ -5,37 +5,81 @@ import EditarProfile from "../SetProfileComp"
 import GETIMG from "@/funcoes/getfoto"
 import toBlob from "@/funcoes/transformBlob"
 import Image from "next/image"
+import POST from "@/funcoes/adicionar"
+import { auth } from "@/auth"
+import GET from "@/funcoes/coletaer"
+import DELETE from "@/funcoes/deletar"
+import GETALL from "@/funcoes/getall"
 
-export default function CompPAGE({ perfilID, dataP }: { perfilID: string, dataP: any }) {
+export default function CompPAGE({ perfilID, dataP, ID }: { perfilID: string, dataP: any, ID: any }) {
     const [estado, setEstado] = useState("Normal")
+    const [seguindo, setSeguindo] = useState(0)
     const [data, setData] = useState({
         nome: dataP.name,
         funcao: dataP.funcao,
         frase: dataP.frase,
         imgProfile: dataP.imageProfile,
-        imgBanner: dataP.imageBanner
+        imgBanner: dataP.imageBanner,
+        seguindo:0,
+        seguidores:0
     })
     const [links, setLinks] = useState({ profile: "", banner: "" })
-    // console.log(dataP
     const GETimg = useCallback(async () => {
         const profile = await GETIMG(data.imgProfile, "Profiles")
         const banner = await GETIMG(data.imgBanner, "Banners")
-        const profilelink = URL.createObjectURL(await toBlob(profile, data.imgProfile.split(".")[1]))
-        const bannerlink = URL.createObjectURL(await toBlob(banner, data.imgBanner.split(".")[1]))
+        const profilelink = URL.createObjectURL(await toBlob(profile, data.imgProfile !== null ? data.imgProfile.split(".")[1] : ".png"))
+        const bannerlink = URL.createObjectURL(await toBlob(banner, data.imgBanner !== null ? data.imgBanner.split(".")[1] : ".png"))
         setLinks({ profile: profilelink, banner: bannerlink })
     }, [data.imgProfile, data.imgBanner])
 
+    //realiza o seguimento (isolar depois)
+    const Seguir = async (e: any) => {
+        console.log(ID, dataP.id)
+        const resp = await POST("Seguidores", ID, dataP.id)
+        if (resp === true) {
+            setSeguindo(1)
+        }
+    }
+    //desfaz o seguir (isolar depois)
+    const Desfazer = async(e:any)=>{
+        const resp = await DELETE("seguidores",ID,dataP.id)
+        if(resp===true){
+            setSeguindo(0)
+        }
+    }
+
+    //verifica se o usuario está seguindo a conta (isolar depois)
+    const IsSeguindo = async () => {
+        const resp = await GET("Seguidores", [ID, dataP.id])
+        if (resp === true) {
+            setSeguindo(1)
+        }
+    }
+
+    //pega os dados de quantos seguidores e quantos seguindo
+    const GETallfollows = async ()=>{
+        const seguidoresF = await GETALL("Seguidores",dataP.id,"userID")
+        const seguindoF = await GETALL("seguidores",dataP.id,"seguidorID")
+        if(seguidoresF!==false && seguindoF!==false){
+            setData((prev)=>({...prev,seguidores:seguidoresF,seguindo:seguindoF}))
+        }
+    }
+
     useEffect(() => {
         GETimg()
+        GETallfollows()
+        if (perfilID !== dataP.userID) {
+            IsSeguindo()
+        }
     }, [])
 
     //vai mostrar o perfil do usuario (caso seja o perfil do proprio usuario vai permitir ele mudar, caso contrario so ler])
     return (
 
-        <div style={{ width: "108.8%", marginLeft: "5.7%", marginTop: "-7.3%" }}>
+        <div style={{ width: "108.8%", marginLeft: "5.7%", marginTop: "-1.3%" }}>
             {estado !== "Normal" ? <EditarProfile props={{ Dados: setData, Estado: setEstado, UserData: data, id: perfilID }} /> : null}
             <div className="BannerPicture">
-                <Image alt="Foto do banner" src={links.banner} width={100} height={100} className="BannerPic" />
+                <Image alt="Foto do banner" src={links.banner} width={120} height={100} className="BannerPic" />
             </div>
             <div className="ProfilePicture">
                 <Image alt="foto do perfil" src={links.profile} width={100} height={100} className="ProfilePic" />
@@ -58,7 +102,7 @@ export default function CompPAGE({ perfilID, dataP }: { perfilID: string, dataP:
                     </div>
                     <div id="ThirdBio">
 
-                        <div id="flexBottom" ><h1>{2} seguidores</h1><h1>{2}seguindo</h1></div>
+                        <div id="flexBottom" ><h1>{data.seguidores} seguidores</h1><h1>{data.seguindo} seguindo</h1></div>
 
                         {perfilID === dataP?.userID ?
                             <div>
@@ -67,9 +111,15 @@ export default function CompPAGE({ perfilID, dataP }: { perfilID: string, dataP:
                                 </div>
                             </div>
                             :
-                            <div className="ButtonBlue" id="botao">
-                                Seguir
-                            </div>
+                            <>
+                                {seguindo === 0 ?
+                                    <div className="ButtonBlue" id="botao" onClick={(e) => { Seguir(e) }}>
+                                        Seguir
+                                    </div>
+                                    : <div className="ButtonBlue" id="botao" onClick={(e) => { Desfazer(e) }}>
+                                        Seguindo
+                                    </div>}
+                            </>
                         }
                     </div>
                 </div>
